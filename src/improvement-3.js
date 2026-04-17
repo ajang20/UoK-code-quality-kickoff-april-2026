c6a17761ef4e60e261ecff4f5ed4653a5c96067a
ajang20 <ajangkorenilo8@gmail.com>
2026-04-17 17:03:08 +0200

    Password validation

--- src/improvement-3.js
+++ src/improvement-3.js
@@ -1,4 +1,6 @@
 /**
+ * // forex-link- https://www.google.com/search?q=are+there+any+free+applications+that+one+can+use+for+backtesting+forex&oq=are+there+any+free+applications+that+one+can+use+for+backtesting+forex&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTE2MjUzajBqN6gCALACAA&sourceid=chrome&ie=UTF-8
+
  *  TRY TO IMPROVE THE FOLLOWING FUNCTION.
  *  YOU CAN REPLACE THE CODE INSIDE THE FUNCTIONS, THE FUNCTION NAME, ETC.
  *  PLEASE DO NOT REMOVE `export default function`
@@ -18,4 +20,3 @@ export default function findLargestNumber(numbers) {
 }
 
 
-// forex-link- https://www.google.com/search?q=are+there+any+free+applications+that+one+can+use+for+backtesting+forex&oq=are+there+any+free+applications+that+one+can+use+for+backtesting+forex&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTE2MjUzajBqN6gCALACAA&sourceid=chrome&ie=UTF-8
--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -19,9 +19,52 @@ export default function isValidPassword(password = "") {
    * if (is not composed by digits and numbers) {
    *   return ...;
    * }
+   
    */
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
-  return true;
+
+//length
+if(password.length !== 10) return false
+
+//lower character & upperCharacter & one number
+const lowerRegex = /[a-z]/
+const upperRegex = /[A-Z]/
+const numRegex = /[0-9]/
+const specialRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/
+
+if(!lowerRegex.test(password) || 
+   !upperRegex.test(password) ||
+   !numRegex.test(password)   ||
+   specialRegex.test(password)) {
+      return false; 
+   }
+   
+   //password doesn't have ascending or descending order of numbers
+   if(!checkingNumberOrder(password)) return false
+
+ 
+  return true
+}
+
+function  checkingNumberOrder(password){
+   const numberRegex = /\d+/g
+   const numberPatterns = password.match(numberRegex)
+   
+   for(const num of numberPatterns){
+       if(num.length) continue;
+       const arr = [...num]
+       const asc =  arr.every((val,i) => i===0 || val > arr[i-1])
+       const desc =  arr.every((val,i) => i===0 || val < arr[i-1])
+       
+       
+       
+       const monotonic = asc || desc ;
+       
+       if(monotonic) return false
+   }
+    const noDuplicates = [new Set(...password)]
+     
+   return true
 }
 
